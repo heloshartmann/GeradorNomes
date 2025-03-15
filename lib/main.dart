7a2e4478ebc527b85ef9f05556d7274d709e1b6d
heloshartmann <133721438+heloshartmann@users.noreply.github.com>
2025-03-14 21:06:13 -0300

    gerador

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,259 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:faker/faker.dart';
+import 'package:flutter/services.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: dotenv.env['API_KEY']!,
+      appId: dotenv.env['APP_ID']!,
+      messagingSenderId: dotenv.env['MESSAGING_SENDER_ID']!,
+      projectId: dotenv.env['PROJECT_ID']!,
+      databaseURL: dotenv.env['DATABASE_URL']!,
+    ),
+  );
+
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final DatabaseReference _database = FirebaseDatabase.instance.ref();
+  List<Map<String, dynamic>> _nomes = [];
+  bool _mostrarFavoritos = false;
+  final Faker _faker = Faker();
+
+  @override
+  void initState() {
+    super.initState();
+    _getNomes();
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  Future<void> _getNomes() async {
+    _database.onValue.listen((event) {
+      final data = event.snapshot.value;
+      if (data != null && data is Map) {
+        List<Map<String, dynamic>> listaNomes = [];
+        data.forEach((key, value) {
+          listaNomes.add({
+            'id': key,
+            'nome': value['nome'],
+            'sobrenome': value['sobrenome'],
+            'favorito': value['favorito'] ?? false,
+          });
+        });
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+        setState(() {
+          _nomes = listaNomes;
+        });
+      } else {
+        setState(() {
+          _nomes = [];
+        });
+      }
     });
   }
 
+  Future<void> _gerarNomeAleatorio() async {
+    String id = _database.push().key ?? "0";
+    String nome = _faker.person.firstName();
+    String sobrenome = _faker.person.lastName();
+
+    await _database.child(id).set({'nome': nome, 'sobrenome': sobrenome});
+
+    _getNomes();
+  }
+
+  Future<void> _deletarNome(String id) async {
+    await _database.child(id).remove();
+    _getNomes();
+  }
+
+  Future<void> _deletarTodos() async {
+    await _database.remove();
+    _getNomes();
+  }
+
+  Future<void> _copiarNome(String nome, String sobrenome) async {
+    final text = "$nome $sobrenome";
+    await Clipboard.setData(ClipboardData(text: text));
+  }
+
+  Future<void> _favoritarNome(String id, bool isFavorito) async {
+    await _database.child(id).update({'favorito': !isFavorito});
+    _getNomes();
+  }
+
+  List<Map<String, dynamic>> _getFavoritos() {
+    return _nomes.where((nome) => nome['favorito'] == true).toList();
+  }
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Gerador de Nomes",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 1.9, 
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+          centerTitle: true,
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Colors.blueAccent,
+                  Colors.deepPurple,
+                ], // Gradiente bonito
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
             ),
-          ],
+          ),
+          elevation: 4, 
+          shadowColor: Colors.black,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ElevatedButton.icon(
+                    onPressed: () {
+                      setState(() {
+                        _mostrarFavoritos = !_mostrarFavoritos;
+                      });
+                    },
+                    icon: Icon(
+                      Icons.star,
+                      color: _mostrarFavoritos ? Colors.grey : Colors.yellow,
+                    ),
+                    label: Text(
+                      _mostrarFavoritos ? "Mostrar Todos" : "Favoritos",
+                    ),
+                  ),
+                  ElevatedButton.icon(
+                    onPressed: _deletarTodos,
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.amber,
+                      foregroundColor: Colors.white,
+                    ),
+                    icon: Icon(Icons.delete, color: Colors.red),
+                    label: Text("Limpar"),
+                  ),
+                ],
+              ),
+              SizedBox(height: 30),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: _gerarNomeAleatorio,
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white, // Cor opcional
+                      foregroundColor: Colors.blue,
+                      padding: EdgeInsets.symmetric(
+                        vertical: 20,
+                        horizontal: 40,
+                      ), 
+                      textStyle: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ), 
+                    ),
+                    child: Text("Gerar Nome Aleatório"),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20),
+              Expanded(
+                child: ListView.builder(
+                  itemCount:
+                      _mostrarFavoritos
+                          ? _getFavoritos().length
+                          : _nomes.length,
+                  itemBuilder: (context, index) {
+                    var lista = _mostrarFavoritos ? _getFavoritos() : _nomes;
+                    bool isFavorito = _nomes[index]['favorito'] ?? false;
+                    return Card(
+                      child: ListTile(
+                        title: Text(
+                          "${lista[index]['nome']} ${lista[index]['sobrenome']}",
+                        ),
+                        trailing: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            IconButton(
+                              icon: Icon(Icons.delete, color: Colors.red),
+                              onPressed: () => _deletarNome(lista[index]['id']),
+                            ),
+                            IconButton(
+                              onPressed: () {
+                                _copiarNome(
+                                  lista[index]['nome'],
+                                  lista[index]['sobrenome'],
+                                );
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(content: Text("Nome copiado!")),
+                                );
+                              },
+                              icon: Icon(Icons.copy),
+                            ),
+                            IconButton(
+                              onPressed: () {
+                                _favoritarNome(
+                                  lista[index]['id'],
+                                  _nomes[index]['favorito'],
+                                );
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(
+                                    content: Text(
+                                      lista[index]['favorito']
+                                          ? "Removido dos favoritos!"
+                                          : "Adicionado aos favoritos!",
+                                    ),
+                                  ),
+                                );
+                              },
+                              icon: Icon(
+                                Icons.star,
+                                color: isFavorito ? Colors.yellow : Colors.grey,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
